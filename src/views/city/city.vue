94eb76164753c9a97996165e25dadbf451bd3f0d
hanwu <hanwu1314@outlook.com>
2023-09-04 10:14:51 +0800

    城市页tab标签搭建

--- src/views/city/city.vue
+++ src/views/city/city.vue
@@ -1,23 +1,27 @@
 <script setup>
+import router from '@/router';
+
+const cancelClick = () => {
+    router.back()
+}
+
+
+/**tab的切换 */
+// const tabActive = ref()
 
 </script>
 <template>
-    <div class="city">
-        <h2>city</h2>
-        <template v-for="item in 100">
-            <div>
-                aaaaaa{{ item }}
-            </div>
-        </template>
+    <div class="city top-page">
+        <!-- 1.搜索框 -->
+        <van-search v-model="searchValue" placeholder="城市/区域/位置" shape="round" show-action @cancel="cancelClick" />
+
+
+        <!-- 2.tab的切换 -->
+        <!-- tabActive默认索引 -->
+        <van-tabs v-model:active="active" color="#ff9854">
+            <van-tab title="国内·港澳台"></van-tab>
+            <van-tab title="海外"></van-tab>
+        </van-tabs>
     </div>
 </template>
-<style lang='less' scoped>
-.city {
-    position: relative;
-    z-index: 9;
-    height: 100vh;
-    background-color: #fff;
-
-    overflow-y: auto;
-}
-</style>
+<style lang='less' scoped></style>
