73248c9137586c05c74c882fe416b6c5fba652b0
hanwu <hanwu1314@outlook.com>
2023-09-04 15:17:37 +0800

    首页-人数价格区域

--- src/views/home/components/home-search-box.vue
+++ src/views/home/components/home-search-box.vue
@@ -76,6 +76,15 @@ const onConfirm = (value) => {
         </div>
         <van-calendar v-model:show="showCalendar" type="range" color="#ff9854" :round="false" :show-confirm="false"
             @confirm="onConfirm" />
+
+        <!-- 价格/人数选择 -->
+        <div class="section price-counter bottom-gray-line">
+            <div class="start">价格不限</div>
+            <div class="end">人数不限</div>
+        </div>
+        <!-- 关键字 -->
+        <div class="section keyword bottom-gray-line">关键字/位置/民宿名</div>
+
     </div>
 </template>
 <style lang='less' scoped>
@@ -161,6 +170,12 @@ const onConfirm = (value) => {
         text-align: center;
         font-size: 12px;
         color: #666;
+    }
+}
+
+.price-counter {
+    .start {
+        border-right: 1px solid var(--line-color);
     }
 }
 </style>
