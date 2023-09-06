8a61715a6d1cc659f8ffe8693e1170f337f1de2c
hanwu <hanwu1314@outlook.com>
2023-09-06 20:32:38 +0800

    1

--- src/components/tab-bar/tab-bar.vue
+++ src/components/tab-bar/tab-bar.vue
@@ -1,23 +1,13 @@
 <script setup>
 import tabbarData from "@/assets/data/tabbar.js"
 import { getAssetURL } from "@/utils/load_assets.js"
-import { ref, watch } from "vue";
-import { useRoute } from "vue-router";
+import { ref } from "vue";
 
-// 监听路由改变时, 找到对应的索引, 设置currentIndex
-const route = useRoute()
 const currentIndex = ref(0)
-watch(route, (newRoute) => {
-    const index = tabbarData.findIndex(item => item.path === newRoute.path)
-    if (index === -1) return
-    currentIndex.value = index
-})
-
-
 </script>
 <template>
     <div class="tab-bar">
-        <van-tabbar v-model="currentIndex" active-color="#ff9854" route>
+        <van-tabbar v-model="currentIndex" active-color="#ff9854">
             <template v-for="(item, index) in tabbarData">
                 <van-tabbar-item :to="item.path">
                     <template #default>
