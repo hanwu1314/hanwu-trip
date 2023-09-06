03301bfc7827411e806ef2478b5dce7e7ae40985
hanwu <hanwu1314@outlook.com>
2023-09-06 20:12:57 +0800

    修复tabber切换路由默认索引不更新

--- src/components/tab-bar/tab-bar.vue
+++ src/components/tab-bar/tab-bar.vue
@@ -1,13 +1,23 @@
 <script setup>
 import tabbarData from "@/assets/data/tabbar.js"
 import { getAssetURL } from "@/utils/load_assets.js"
-import { ref } from "vue";
+import { ref, watch } from "vue";
+import { useRoute } from "vue-router";
 
+// 监听路由改变时, 找到对应的索引, 设置currentIndex
+const route = useRoute()
 const currentIndex = ref(0)
+watch(route, (newRoute) => {
+    const index = tabbarData.findIndex(item => item.path === newRoute.path)
+    if (index === -1) return
+    currentIndex.value = index
+})
+
+
 </script>
 <template>
     <div class="tab-bar">
-        <van-tabbar v-model="currentIndex" active-color="#ff9854">
+        <van-tabbar v-model="currentIndex" active-color="#ff9854" route>
             <template v-for="(item, index) in tabbarData">
                 <van-tabbar-item :to="item.path">
                     <template #default>
